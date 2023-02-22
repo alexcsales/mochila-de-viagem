ff7b32c891306f481b962dc3d0337f9eb1196f8d
Alex <alexcsalesdev@gmail.com>
2023-02-22 10:43:39 -0300

    adicionado: logica de atualizar itens

--- js/main.js
+++ js/main.js
@@ -3,28 +3,41 @@ const lista = document.getElementById('lista');
 const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
 itens.forEach((elemento) => {
-    criaItem(elemento)
+    criaItem(elemento);
 })
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+    console.log(existe)
 
     const item = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaItem(item)
+    if (existe) {
+        item.id = existe.id;
 
-    itens.push(item)
+        atualizaItem(item);
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+        itens[existe.id] = item;
+    } else {
+        item.id = itens.length;
 
-    nome.value = ""
-    quantidade.value = ""
+        criaItem(item);
+
+        itens.push(item);
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+    quantidade.value = "";
 })
 
 function criaItem(item) {
@@ -34,10 +47,16 @@ function criaItem(item) {
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
 
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
 
+}
+
+function atualizaItem(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
+    console.log(document.querySelector("[data-id='" + item.id + "']"))
 }
